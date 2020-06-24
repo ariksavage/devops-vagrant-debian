2cb9ac8bd471d5c31245c163a2ca74d9f93ca0f7
Arik Savage <ariksavage@MacBook-Pro.local>
2020-06-24 16:54:34 -0500

    install and configure drush for drupal sites

--- provision/cms/drupal.sh
+++ provision/cms/drupal.sh
@@ -9,6 +9,12 @@ pwd
 if [ -f "./composer.json" ]; then
   composer diagnose
   composer self-update
-  echo "Composer install - This might take a while..."
-  composer install -vvv
+  # echo "Composer install - This might take a while..."
+  # composer install -vvv
+  # composer drupal:scaffold
+  echo "Install Drush"
+  composer global require drush/drush -vvv
+  vendor/bin/drush init -y
+
+  # move drush aliases?
 fi
